53e0f9cdee5770e8ce9fca45a0f8aae1379c7508
Bonface <bonniewanga34@gmail.com>
2023-09-15 19:36:15 +0300

    Completed assignments

--- src/components/ColdStorage.js
+++ src/components/ColdStorage.js
@@ -7,6 +7,7 @@ function ColdStorage() {
       <Segment compact>
         <h3 className="labels">ColdStorage</h3>
       </Segment>
+      
       <Segment compact>
         {/* Cold Storage contains hosts....but how? Directly? Or is there something else we could use to contain them... */}
       </Segment>
